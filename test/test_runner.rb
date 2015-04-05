a281a8faafccbb9c914504f418d0d9774d3e53c5
Thibaut Barrère <thibaut.barrere@gmail.com>
2015-04-05 11:44:13 +0200

    Add test for block transform processing

---
+++ test/test_runner.rb
@@ -0,0 +1,19 @@
+require_relative 'helper'
+
+require_relative 'support/test_enumerable_source'
+
+class TestRunner < Kiba::Test
+  let(:control) do
+    control = Kiba::Control.new
+    # this will yield a single row for testing
+    control.sources << {klass: TestEnumerableSource, args: [[{field: 'value'}]]} 
+    control
+  end
+
+  def test_block_transform_processing
+    # is there a better way to assert a block was called in minitest?
+    control.transforms << lambda { |r| @called = true; r }
+    Kiba.run(control)
+    assert_equal true, @called
+  end
+end
