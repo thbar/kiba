743c8f71d6f4c5c00ac0532785304119b5d2b52a
Thibaut Barrère <thibaut.barrere@gmail.com>
2015-04-22 00:44:27 +0200

    Split the run method to reduce complexity a bit

--- lib/kiba/runner.rb
+++ lib/kiba/runner.rb
@@ -1,6 +1,7 @@
 module Kiba
   module Runner
     def run(control)
+      # instantiate early so that error are raised before any processing occurs
       pre_processes = to_instances(control.pre_processes, true, false)
       sources = to_instances(control.sources)
       destinations = to_instances(control.destinations)
@@ -8,10 +9,17 @@ module Kiba
       post_processes = to_instances(control.post_processes, true, false)
 
       pre_processes.each(&:call)
-
+      process_rows(sources, transforms, destinations)
+      destinations.each(&:close)
+      post_processes.each(&:call)
+    end
+    
+    def process_rows(sources, transforms, destinations)
       sources.each do |source|
         source.each do |row|
           transforms.each do |transform|
+            # TODO: avoid the case completely by e.g. subclassing Proc
+            # and aliasing `process` to `call`. Benchmark needed first though.
             if transform.is_a?(Proc)
               row = transform.call(row)
             else
@@ -25,9 +33,6 @@ module Kiba
           end
         end
       end
-
-      destinations.each(&:close)
-      post_processes.each(&:call)
     end
 
     # not using keyword args because JRuby defaults to 1.9 syntax currently
