78e0362791c7ced6487493a39d7bb8779f2ac988
Thibaut Barrère <thibaut.barrere@gmail.com>
2017-04-16 12:02:33 +0200

    Add tests for yielding transform

--- test/test_alternate_runner.rb
+++ test/test_alternate_runner.rb
@@ -1,6 +1,8 @@
 require_relative 'helper'
 require 'minitest/mock'
 require_relative 'support/test_enumerable_source'
+require_relative 'support/test_array_destination'
+require_relative 'support/test_yielding_transform'
 require_relative 'common/runner'
 
 class TestAlternateRunner < Kiba::Test
@@ -10,5 +12,33 @@ class TestAlternateRunner < Kiba::Test
     runner = Object.new
     runner.extend(Kiba::AlternateRunner)
     runner.run(job)
+  end
+  
+  
+  def test_yielding_class_transform
+    input_row = {tags: ["one", "two", "three"]}
+    
+    control = Kiba::Control.new
+    control.sources << {
+      klass: TestEnumerableSource,
+      args: [[input_row]]
+    }
+    control.transforms << { 
+      klass: TestYieldingTransform
+    }
+    array = []
+    control.destinations << {
+      klass: TestArrayDestination,
+      args: [array]
+    }
+    
+    kiba_run(control)
+  
+    assert_equal [
+      {item: 'one'},
+      {item: 'two'},
+      {item: 'three'},
+      {item: 'classic-return-value'}
+    ], array
   end
 end
