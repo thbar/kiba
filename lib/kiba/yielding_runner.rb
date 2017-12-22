d30cc0bce0e3bc4a98f9a0f2c5be8b78f1f10fb4
Thibaut Barrère <thibaut.barrere@gmail.com>
2017-12-22 23:35:22 +0100

    Implement YieldingRunner
    This cherry-picks from #41 but with a more DRY code.

---
+++ lib/kiba/yielding_runner.rb
@@ -0,0 +1,35 @@
+module Kiba
+  module YieldingRunner
+    include Runner
+    
+    def lazy_transform(from, t)
+      Enumerator::Lazy.new(from) do |yielder, input_row|
+        final_row = t.process(input_row) do |yielded_row|
+          yielder << yielded_row
+        end
+        yielder << final_row if final_row
+      end
+    end
+
+    def process_rows(sources, transforms, destinations)
+      source_rows = Enumerator::Lazy.new(sources) do |yielder, source|
+        source.each do |row|
+          yielder << row
+        end
+      end
+
+      rows = source_rows.lazy
+      
+      transforms.each do |transform|
+        rows = lazy_transform(rows, transform)
+      end
+      
+      rows.each do |row|
+        destinations.each do |destination|
+          destination.write(row)
+        end
+      end
+      destinations.find_all { |d| d.respond_to?(:close) }.each(&:close)
+    end
+  end
+end
