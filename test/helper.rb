da0887aa4249d7304abdca65e2ca5e58dffd89d8
Thibaut Barrère <thibaut.barrere@gmail.com>
2018-05-13 23:40:46 +0200

    Remove dependency on Minitest::Spec::DSL (#55)
    I found this confusing while re-reading some code, so I'm now removing it.

--- test/helper.rb
+++ test/helper.rb
@@ -8,8 +8,6 @@ if ENV['CI'] == 'true'
 end
 
 class Kiba::Test < Minitest::Test
-  extend Minitest::Spec::DSL
-
   def remove_files(*files)
     files.each do |file|
       File.delete(file) if File.exist?(file)
