cff28f0d51d29a0cae2288e7b05e9a6dabc3e2a1
Thibaut Barrère <thibaut.barrere@gmail.com>
2020-02-04 19:29:38 +0100

    Make component usable as source/transform/destination

--- test/support/test_keyword_arguments_component.rb
+++ test/support/test_keyword_arguments_component.rb
@@ -1,11 +1,11 @@
-class TestKeywordArgumentsSource
+# a mock component to test Ruby 3 keyword argument support
+class TestKeywordArgumentsComponent
   def initialize(mandatory:, optional: nil)
     @mandatory = mandatory
     @optional = optional
   end
   
   def each
-    yield @mandatory
-    yield @optional if @optional
+    # no-op
   end
 end
