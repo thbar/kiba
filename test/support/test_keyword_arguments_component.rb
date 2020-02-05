16caefa47a685ae267a65ce9827ed43666eda837
Thibaut Barrère <thibaut.barrere@gmail.com>
2020-02-05 14:45:57 +0100

    Add more tests & make them shared
    (shared between the streaming runner and the legacy runner)

--- test/support/test_keyword_arguments_component.rb
+++ test/support/test_keyword_arguments_component.rb
@@ -1,8 +1,11 @@
 # a mock component to test Ruby 3 keyword argument support
 class TestKeywordArgumentsComponent
-  def initialize(mandatory:, optional: nil)
-    @mandatory = mandatory
-    @optional = optional
+  def initialize(mandatory:, optional: nil, on_init: nil)
+    values = {
+      mandatory: mandatory,
+      optional: optional
+    }
+    on_init&.call(values)
   end
   
   def each
