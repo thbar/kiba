efccd4d2fc80b4746644d12b3ad1ec7f8f3d618d
Thibaut Barrère <thibaut.barrere@gmail.com>
2020-10-16 15:10:09 +0200

    Implement change

--- lib/kiba.rb
+++ lib/kiba.rb
@@ -11,7 +11,13 @@ require 'kiba/dsl_extensions/config'
 Kiba.extend(Kiba::Parser)
 
 module Kiba
-  def self.run(job)
+  def self.run(job = nil, &block)
+    unless (job.nil? ^ block.nil?)
+      fail ArgumentError.new("Kiba.run takes either one argument (the job) or a block (defining the job)")
+    end
+
+    job ||= Kiba.parse { instance_exec(&block) }
+
     # NOTE: use Hash#dig when Ruby 2.2 reaches EOL
     runner = job.config.fetch(:kiba, {}).fetch(:runner, Kiba::StreamingRunner)
     runner.run(job)
